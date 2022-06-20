28219d990d3b909fc37d4197abfe0e888b687a99
Thiago Andrade <thiago.adsix@gmail.com>
2022-06-20 00:01:53 -0300

    feat: initial commit

---
+++ src/main/kotlin/app/financialselfmanagement/FinancialSelfManagementApplication.kt
@@ -0,0 +1,11 @@
+package app.financialselfmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialSelfManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialSelfManagementApplication>(*args)
+}
