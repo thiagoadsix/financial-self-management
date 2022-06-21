a0426ab04811fb824323143dd09e76237374e60a
thiagoadsix <thiago.adsix@gmail.com>
2022-06-21 02:15:41 -0300

    feat: inject repository into purchase service

--- src/main/kotlin/app/financialselfmanagement/services/PurchaseService.kt
+++ src/main/kotlin/app/financialselfmanagement/services/PurchaseService.kt
@@ -1,8 +1,14 @@
 package app.financialselfmanagement.services
 
+import app.financialselfmanagement.entities.PurchaseEntity
+import app.financialselfmanagement.repositories.PurchaseRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PurchaseService {
-    fun purchase() {}
+class PurchaseService (
+    private var purchaseRepository: PurchaseRepository
+) {
+    fun purchase(data: PurchaseEntity) {
+        purchaseRepository.save(data)
+    }
 }
