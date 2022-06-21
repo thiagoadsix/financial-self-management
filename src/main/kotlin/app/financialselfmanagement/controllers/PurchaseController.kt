1662fa820c63cf3d3c2cfb3215071d768a712d02
thiagoadsix <thiago.adsix@gmail.com>
2022-06-21 10:18:47 -0300

    refactor: add purchase mapper to controller

--- src/main/kotlin/app/financialselfmanagement/controllers/PurchaseController.kt
+++ src/main/kotlin/app/financialselfmanagement/controllers/PurchaseController.kt
@@ -1,17 +1,21 @@
 package app.financialselfmanagement.controllers
 
+import app.financialselfmanagement.mappers.PurchaseMapper
+import app.financialselfmanagement.request.PostPurchaseRequest
 import app.financialselfmanagement.usescases.PurchaseUsecase
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("purchases")
 class PurchaseController (
-    private val purchaseUsecase: PurchaseUsecase
+    private val purchaseUsecase: PurchaseUsecase,
+    private val purchaseMapper: PurchaseMapper
 ) {
-    @GetMapping()
-    fun purchase() {
-        purchaseUsecase.purchase()
+    @PostMapping()
+    fun purchase(@RequestBody request: PostPurchaseRequest) {
+        purchaseUsecase.purchase(purchaseMapper.toModel(request))
     }
 }
