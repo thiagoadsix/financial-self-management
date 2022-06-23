580067a8f37444006c910055fe8df083fb8c08db
thiagoadsix <thiago.adsix@gmail.com>
2022-06-23 00:29:05 -0300

    chore: add gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,12 +32,16 @@ apply(plugin = "org.flywaydb.flyway")
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.flywaydb:flyway-core")
+	implementation("com.google.code.gson:gson:2.9.0")
+
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
